c062c59e172b52ad688654a8701338ccf267b10c
Felix Amenya <kenyansafelix@gmail.com>
2024-07-15 23:03:16 +0300

    homescreen

--- app/src/main/java/com/HomeScreen.kt
+++ app/src/main/java/com/HomeScreen.kt
@@ -1,17 +1,20 @@
 package com
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun HomeScreen(){
     Surface (
         modifier = Modifier.fillMaxSize()) {
-
+        ConstraintLayout (
+            modifier = Modifier.fillMaxSize()){
+            val (nameRow, list, card, topBar) = createRefs()
+        }
     }
 
 }

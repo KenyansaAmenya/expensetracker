20bbdbd8787f4bc81df6a99f447d393e7953bc2d
Felix Amenya <kenyansafelix@gmail.com>
2024-07-15 22:18:39 +0300

    Initial commit

---
+++ app/src/main/java/com/HomeScreen.kt
@@ -0,0 +1,15 @@
+package com
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+
+}
+
+@Composable
+@Preview
+fun PreviewHomeScreen(){
+    HomeScreen()
+}

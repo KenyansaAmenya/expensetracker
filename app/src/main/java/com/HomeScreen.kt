65208c4796670e0a129e6fe2dea7668f8dd41589
Felix Amenya <kenyansafelix@gmail.com>
2024-07-16 23:46:33 +0300

    homescreen

--- app/src/main/java/com/HomeScreen.kt
+++ app/src/main/java/com/HomeScreen.kt
@@ -1,11 +1,28 @@
 package com
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.kenyansa.expensetracker.R
 
 @Composable
 fun HomeScreen(){
@@ -14,9 +31,59 @@ fun HomeScreen(){
         ConstraintLayout (
             modifier = Modifier.fillMaxSize()){
             val (nameRow, list, card, topBar) = createRefs()
+            Image(painter = painterResource(id = R.drawable.ic_topbar), contentDescription = null,
+                modifier = Modifier.constrainAs(topBar) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
+            Box (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+                    .constrainAs(nameRow) {
+                        top.linkTo(parent.top)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    }
+            ){
+                Column {
+                    Text(text = "Good Afternoon", fontSize = 16.sp, color = Color.White)
+                    Text(text = "KenyansaCodes",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                }
+
+                Image(painter = painterResource(id = R.drawable.dots_menu),
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                    )
+            }
+            CardItem(modifier = Modifier
+                .constrainAs(card){
+                    top.linkTo(nameRow.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                })
         }
     }
+}
 
+@Composable
+fun CardItem(modifier: Modifier){
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.Green)
+    ) {
+
+
+    }
 }
 
 @Composable

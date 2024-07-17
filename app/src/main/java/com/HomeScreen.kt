75b98350028e4a9f0255c5aef0ee99b00b5a682d
Felix Amenya <kenyansafelix@gmail.com>
2024-07-17 22:35:16 +0300

    homescreen ui

--- app/src/main/java/com/HomeScreen.kt
+++ app/src/main/java/com/HomeScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -23,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.kenyansa.expensetracker.R
+import com.kenyansa.expensetracker.ui.theme.Equity
 
 @Composable
 fun HomeScreen(){
@@ -40,7 +44,7 @@ fun HomeScreen(){
             Box (
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 32.dp, start = 16.dp, end = 16.dp)
+                    .padding(top = 64.dp, start = 16.dp, end = 16.dp)
                     .constrainAs(nameRow) {
                         top.linkTo(parent.top)
                         start.linkTo(parent.start)
@@ -56,7 +60,7 @@ fun HomeScreen(){
                     )
                 }
 
-                Image(painter = painterResource(id = R.drawable.dots_menu),
+                Image(painter = painterResource(id = R.drawable.ic_notification),
                     contentDescription = null,
                     modifier = Modifier.align(Alignment.CenterEnd)
                     )
@@ -77,14 +81,81 @@ fun CardItem(modifier: Modifier){
         modifier = modifier
             .padding(16.dp)
             .fillMaxWidth()
-            .padding(16.dp)
+            .height(200.dp)
             .clip(RoundedCornerShape(16.dp))
-            .background(Color.Green)
+            .background(Equity)
+            .padding(16.dp)
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)) {
+            Column (modifier = Modifier.align(Alignment.CenterStart)) {
+                Text(text = "Total Balance", fontSize = 16.sp, color = Color.White)
+                Text(
+                    text = "Kshs 500,000",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
+                )
+            }
+            Image(painter = painterResource(id = R.drawable.dots_menu),
+                contentDescription = null,
+                modifier = Modifier.align(Alignment.CenterEnd)
+            )
+        }
 
-
+        Box (
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ){
+            CardRowItem(
+              modifier = Modifier
+                  .align(Alignment.CenterStart),
+              title = "Income",
+              amount = "Kshs 325,000",
+              image = R.drawable.ic_income)
+            CardRowItem(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd),
+                title = "Expense",
+                amount = "Kshs 150,000",
+                image = R.drawable.ic_expense
+            )
+        }
     }
 }
+
+
+@Composable
+fun TransactionList(
+    modifier: Modifier){
+    Column(modifier = Modifier) {
+        Box(modifier = Modifier.fillMaxWidth()){
+            Text(text = "Recent Transactions", fontSize = 20.sp)
+            Text(text = "See all", fontSize = 16.sp, color = Color.White)
+        }
+    }
+
+}
+
+
+@Composable
+fun CardRowItem(modifier: Modifier, title: String, amount: String, image: Int) {
+
+    Column (modifier = modifier){
+        Row {
+            Image(painter = painterResource(id = image), contentDescription = null
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = title, fontSize = 16.sp, color = Color.White)
+
+        }
+        Text(text = amount, fontSize = 20.sp, color = Color.White)
+    }
+}
+
 
 @Composable
 @Preview(showBackground = true)
